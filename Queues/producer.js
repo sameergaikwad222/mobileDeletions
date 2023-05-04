c5006b7f23f37f0c1c8c8eb3a4b4a89428cc9985
Sameer <sameergaikwad222@gmail.com>
2023-05-04 23:45:20 +0530

    commented console.log

--- Queues/producer.js
+++ Queues/producer.js
@@ -4,7 +4,7 @@ async function pushToProcess(data, queueName, channelObject) {
   try {
     channelObject.sendToQueue(queueName, Buffer.from(data + ""));
     logger.info(`${data} Message sent to Queue ${queueName}.`);
-    console.log(`${data} Message sent to Queue ${queueName}.`);
+    // console.log(`${data} Message sent to Queue ${queueName}.`);
   } catch (error) {
     logger.error(`Error sending message to Queue ${queueName}.`);
     throw error;
